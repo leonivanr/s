11d652d5c2c595c0564e4665690d7cd7d62f8ec0
Leon <leonivanrodrigo@gmail.com>
2019-05-16 02:42:42 -0300

    Creado snake y canvas.

--- app.js
+++ app.js
@@ -1,5 +1,60 @@
-// TODO: 
+// UI Colors
+const canvasBorderColor = 'darkgreen';
+const canvasBackgroundColor = 'orange';
+const snakeBorderColor = 'black';
+const snakeBackgroundColor = 'brown';
+let snake = [{
+        x: 150,
+        y: 150
+    },
+    {
+        x: 140,
+        y: 150
+    },
+    {
+        x: 130,
+        y: 150
+    },
+    {
+        x: 120,
+        y: 150
+    }, 
+    {
+        x: 110,
+        y: 150
+    },
+]
 
-//FIXME:
+// Obtener canvas.
+var canvasGame = document.querySelector('#gameCanvas');
 
-//REVIEW:
+// Setear a 2d.
+var canSnake = canvasGame.getContext('2d');
+
+//Seteamos el color.
+canSnake.fillStyle = canvasBackgroundColor;
+// Seteamos el borde.
+canSnake.strokeStyle = canvasBorderColor;
+
+// Dibujar 
+canSnake.fillRect(0, 0, canvasGame.width, canvasGame.height);
+canSnake.strokeRect(0, 0, canvasGame.width, canvasGame.height);
+
+
+dibujarSerpiente();
+
+//Dibujamos un rectangulo por cada par de coordenadas - cuerpo de snake.
+function dibujarCuerpoSerpiente(snakeCuerpo) {
+    // Damos color y borde a nuestra serpiente.
+    canSnake.fillStyle = snakeBackgroundColor ;
+    canSnake.StrokeStyle = snakeBorderColor ;
+    
+    // Dibujamos con las coordenadas que tomamos de snakeCuerpo.
+    canSnake.fillRect(snakeCuerpo.x, snakeCuerpo.y,10,10);
+    canSnake.strokeRect(snakeCuerpo.x, snakeCuerpo.y, 10, 10);
+}
+
+//
+function dibujarSerpiente() {
+    snake.forEach(dibujarCuerpoSerpiente);
+}
