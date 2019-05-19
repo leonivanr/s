c9afa93e6fd1e2075811373de59d27a9c36280c5
Leon <leonivanrodrigo@gmail.com>
2019-05-19 03:29:50 -0300

    Agregado colisión, puntaje.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const canvasBorderColor = 'darkgreen';
 const canvasBackgroundColor = 'orange';
 const snakeBorderColor = 'black';
 const snakeBackgroundColor = 'brown';
+const obtPuntaje = document.querySelector('#puntaje');
+let puntaje = 0;
 // Definimos donde inicia la serpiente.
 let snake = [{
         x: 150,
@@ -26,6 +28,7 @@ let snake = [{
     },
 ]
 
+// Ejes de donde aparece la comida.
 let comidaX;
 let comidaY;
 
@@ -43,25 +46,26 @@ var canSnake = canvasGame.getContext('2d');
 canSnake.fillStyle = canvasBackgroundColor;
 // Seteamos el borde.
 canSnake.strokeStyle = canvasBorderColor;
+
+
 // Inicio del juego.
 main();
-
+// Genero comida para la primera vez. 
 generarComida();
-
+// Escucho las teclas presionadas.
 addEventListener('keydown', cambiarDireccion);
 
-
 function main() {
+    if (colisionSerpiente()) return;
     setTimeout(function onTick() {
         limpiarLienzo();
         dibujarComida();
         moverSerpiente();
         dibujarSerpiente();
-
+        // Vuelvo a llamar, para que se mueva constantemente.
         main();
     }, 100)
 }
-
 // Limpiamos el lienzo, para eliminiar los rastros dejados por la serpiente.
 function limpiarLienzo() {
 
@@ -94,25 +98,24 @@ function cambiarDireccion(e) {
     const flechaAbajo = 40;
 
     const teclaPresionada = e.keyCode;
-    //NOTE: Verificar si va a quedar sin reverso.
-    const moverDerecha = dx === 10;
-    const moverIzquierda = dx === -10;
-    const moverArriba = dy === -10;
-    const moverAbajo = dy === 10;
-
-    if (teclaPresionada === flechaDerecha) {
+    const yendoDerecha = dx === 10;
+    const yendoIzquierda = dx === -10;
+    const yendoArriba = dy === -10;
+    const yendoAbajo = dy === 10;
+    // 
+    if (teclaPresionada === flechaDerecha && !yendoIzquierda) {
         dx = 10;
         dy = 0;
     }
-    if (teclaPresionada === flechaArriba) {
+    if (teclaPresionada === flechaArriba && !yendoAbajo) {
         dx = 0;
         dy = -10;
     }
-    if (teclaPresionada === flechaIzquierda) {
+    if (teclaPresionada === flechaIzquierda && !yendoDerecha) {
         dx = -10;
         dy = 0;
     }
-    if (teclaPresionada === flechaAbajo) {
+    if (teclaPresionada === flechaAbajo && !yendoArriba) {
         dx = 0;
         dy = 10;
     }
@@ -127,20 +130,31 @@ function moverSerpiente() {
     };
     // Inserto la nueva cabeza al principio del array.
     snake.unshift(cabeza);
-    // Elimino el ultimo elemento del array - la cola.
-    snake.pop()
+    // Si la cabeza de la serpiente toca la comida, la agrego.
+    const tocoComida = snake[0].x === comidaX && snake[0].y === comidaY;
+    if (tocoComida) {
+        puntaje += 10;
+        obtPuntaje.innerHTML = puntaje;
+        generarComida();
+    } else {
+        snake.pop();
+    }
+
+
+    // Elimino el ultimo elemento del array (la cola).
+
 }
 
 function generarAzar(minimo, maximo) {
     return Math.round((Math.random() * (maximo - minimo) + minimo) / 10) * 10;
-    }
+}
 
 function generarComida() {
     comidaX = generarAzar(0, canvasGame.width - 10);
     comidaY = generarAzar(0, canvasGame.height - 10);
     console.log(comidaX);
     console.log(comidaY);
-    
+
     snake.forEach(function laComidaSeCreaEnLaSerpiente(parte) {
         if (comida = parte.x == comidaX && parte.y == comidaY) {
             generarComida();
@@ -152,8 +166,18 @@ function dibujarComida() {
     canSnake.fillStyle = 'blue';
     canSnake.strokeStyle = 'black';
 
-    
+
     canSnake.fillRect(comidaX, comidaY, 10, 10);
     canSnake.strokeRect(comidaX, comidaY, 10, 10);
 
+}
+
+function colisionSerpiente() {
+    // Chequeo si se choca consigo misma.
+    // Empiezo en [4], por que es imposible que se choque con solo 4 partes.
+    for (let i = 4; i < snake.length; i++) {
+        // Snake[0] es la cabeza, por lo tanto nunca cambia.
+        const haChocado = snake[i].x === snake[0].x && snake[i].y === snake[0].y;
+        if(haChocado) return true;
+    }
 }
