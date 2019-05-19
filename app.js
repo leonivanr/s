01e5c18b926348a1d1686b72d220aa6027c51fd0
Leon <leonivanrodrigo@gmail.com>
2019-05-18 21:18:36 -0300

    Agregado generar comida.

--- app.js
+++ app.js
@@ -19,12 +19,15 @@ let snake = [{
     {
         x: 120,
         y: 150
-    }, 
+    },
     {
         x: 110,
         y: 150
     },
 ]
+
+let comidaX;
+let comidaY;
 
 // Velocidad a la que se mueve.
 let dx = 10;
@@ -40,19 +43,24 @@ var canSnake = canvasGame.getContext('2d');
 canSnake.fillStyle = canvasBackgroundColor;
 // Seteamos el borde.
 canSnake.strokeStyle = canvasBorderColor;
+// Inicio del juego.
+main();
+
+generarComida();
 
 addEventListener('keydown', cambiarDireccion);
-main();
+
+
 function main() {
     setTimeout(function onTick() {
         limpiarLienzo();
+        dibujarComida();
         moverSerpiente();
         dibujarSerpiente();
 
         main();
     }, 100)
 }
-
 
 // Limpiamos el lienzo, para eliminiar los rastros dejados por la serpiente.
 function limpiarLienzo() {
@@ -62,22 +70,23 @@ function limpiarLienzo() {
 
     canSnake.fillRect(0, 0, canvasGame.width, canvasGame.height);
     canSnake.strokeRect(0, 0, canvasGame.width, canvasGame.height);
-    
+
 }
 //Dibujamos un rectangulo por cada par de coordenadas - cuerpo de snake.
 function dibujarCuerpoSerpiente(snakeCuerpo) {
     // Damos color y borde a nuestra serpiente.
-    canSnake.fillStyle = snakeBackgroundColor ;
-    canSnake.StrokeStyle = snakeBorderColor ;
-    
+    canSnake.fillStyle = snakeBackgroundColor;
+    canSnake.StrokeStyle = snakeBorderColor;
+
     // Dibujamos con las coordenadas que tomamos de snakeCuerpo.
-    canSnake.fillRect(snakeCuerpo.x, snakeCuerpo.y,10,10);
+    canSnake.fillRect(snakeCuerpo.x, snakeCuerpo.y, 10, 10);
     canSnake.strokeRect(snakeCuerpo.x, snakeCuerpo.y, 10, 10);
 }
 //
 function dibujarSerpiente() {
     snake.forEach(dibujarCuerpoSerpiente);
 }
+
 function cambiarDireccion(e) {
     const flechaDerecha = 39;
     const flechaIzquierda = 37;
@@ -109,11 +118,42 @@ function cambiarDireccion(e) {
     }
 
 }
-function moverSerpiente() { 
+
+function moverSerpiente() {
     //Creo una nueva cabeza para la serpiente.
-    const cabeza = {x: snake[0].x + dx, y: snake[0].y + dy};
+    const cabeza = {
+        x: snake[0].x + dx,
+        y: snake[0].y + dy
+    };
     // Inserto la nueva cabeza al principio del array.
     snake.unshift(cabeza);
     // Elimino el ultimo elemento del array - la cola.
     snake.pop()
+}
+
+function generarAzar(minimo, maximo) {
+    return Math.round((Math.random() * (maximo - minimo) + minimo) / 10) * 10;
+    }
+
+function generarComida() {
+    comidaX = generarAzar(0, canvasGame.width - 10);
+    comidaY = generarAzar(0, canvasGame.height - 10);
+    console.log(comidaX);
+    console.log(comidaY);
+    
+    snake.forEach(function laComidaSeCreaEnLaSerpiente(parte) {
+        if (comida = parte.x == comidaX && parte.y == comidaY) {
+            generarComida();
+        }
+    })
+}
+
+function dibujarComida() {
+    canSnake.fillStyle = 'blue';
+    canSnake.strokeStyle = 'black';
+
+    
+    canSnake.fillRect(comidaX, comidaY, 10, 10);
+    canSnake.strokeRect(comidaX, comidaY, 10, 10);
+
 }
